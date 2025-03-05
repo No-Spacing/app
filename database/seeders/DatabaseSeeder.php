619df4f0a8d1ca258eff595eb6431292133a1f95
Jericho Vega <64142238+No-Spacing@users.noreply.github.com>
2025-03-05 11:50:36 +0800

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\DB;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Faker\Factory as Faker;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,6 +17,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+    
+        $faker = Faker::create();
+        
         DB::table('admins')->insert([
             'username' => 'admin@email.com',
             'password' => hash::make('Admin123'),
@@ -29,7 +33,15 @@ class DatabaseSeeder extends Seeder
         DB::table('video_tables')->insert([
             'video_url' => '/assets/VideoFiles/Chemical.mp4'
         ]);
-
         
+        foreach (range(1, 20) as $index)  {
+            DB::table('products')->insert([
+                'name' => $faker->city,
+                'category' => 'Category '. $faker->numberBetween($min = 1, $max = 4),
+                'description'=> $faker->paragraph($nb = 1),
+                'date_time' => date("Y-m-d\TH:i"),
+            ]);
+        }
+
     }
 }
